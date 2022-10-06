38399592f1a20bd767fcd130c7f2dfc37cbd0f68
SzatmariA <szatma@inf.u-szeged.hu>
2022-10-06 08:37:50 +0200

    write some unittests

---
+++ src/main/python/test/test_line_coverage.py
@@ -0,0 +1,56 @@
+import unittest
+from cov.Line_Coverage import Line_Coverage
+
+
+class TestLineCoverage(unittest.TestCase):
+    def test_clean(self):
+        line_cov = Line_Coverage()
+        line_coverage_with_context = {
+            'C:\\Users\\user\\Documents\\test_me\\example.py': {1: [''], 2: [''], 5: [''], 8: [''], 6: [
+                'test/test_example_e.py::MyTestCase::test_congratulations_for_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 3: [
+                'test/test_example_e.py::MyTestCase::test_error|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 9: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 10: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 11: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 13: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run'], 18: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 19: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 14: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run'], 15: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 16: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run',
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 12: [
+                'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run']},
+            'C:\\Users\\user\\Documents\\test_me\\test\\__init__.py': {1: ['']},
+            'C:\\Users\\user\\Documents\\test_me\\test\\test_example_e.py': {1: [''], 3: ['']
+                , 6: [''], 7: [''], 11: [''], 15: [''], 19: [''], 22: [''], 26: [''], 20: [
+                    'test/test_example_e.py::MyTestCase::test_congratulations_for_10|run'], 23: [
+                    'test/test_example_e.py::MyTestCase::test_error|run'], 24: [
+                    'test/test_example_e.py::MyTestCase::test_error|run'], 16: [
+                    'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run'], 17: [
+                    'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run'], 12: [
+                    'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run'], 13: [
+                    'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run'], 8: [
+                    'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 9: [
+                    'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run']}}
+        expected_clean_dictionary = {'C:\\Users\\user\\Documents\\test_me\\example.py': {6: ['test/test_example_e.py::MyTestCase::test_congratulations_for_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 3: ['test/test_example_e.py::MyTestCase::test_error|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 9: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 10: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 11: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 13: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run'], 18: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 19: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_10|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 14: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run'], 15: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 16: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_11|run', 'test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run'], 12: ['test/test_example_e.py::MyTestCase::test_guess_the_num_with_9|run']}}
+        self.assertEqual(expected_clean_dictionary, line_cov._Line_Coverage__clean(line_coverage_with_context))
+
+if __name__ == '__main__':
+    unittest.main()
