ca59f46648904a7a2ea93393fba29b7bb9a5db53
SzatmariA <szatma@inf.u-szeged.hu>
2022-10-26 14:40:36 +0200

    add interactivity on close context level

--- src/main/java/services/interactivity/Interactivity.java
+++ src/main/java/services/interactivity/Interactivity.java
@@ -1,9 +1,11 @@
 package services.interactivity;
 
-import models.bean.context.CloseContext;
+import java.util.List;
+import models.bean.Formulas;
+import models.bean.ITestData;
 import models.bean.context.FarContext;
 
 public interface Interactivity {
-    public void recalculateCloseContextScores(int line, CloseContext closeContext, Double recalculationFactor);
+    public void recalculateCloseContextScores(ITestData codeElement, Double recalculationFactor, Formulas formula);
     public void recalculateFarContextScores(int line, FarContext farContext, Double recalculationFactor);
 }
--- src/main/java/services/interactivity/StatementInteractivity.java
+++ src/main/java/services/interactivity/StatementInteractivity.java
@@ -1,13 +1,33 @@
 package services.interactivity;
 
-import models.bean.context.CloseContext;
+import java.util.List;
+import models.bean.Formulas;
+import models.bean.ITestData;
+import models.bean.StatementTestData;
 import models.bean.context.FarContext;
 
 public class StatementInteractivity implements Interactivity{
 
     @Override
-    public void recalculateCloseContextScores(int line, CloseContext closeContext, Double recalculationFactor) {
-
+    public void recalculateCloseContextScores(ITestData statement, Double recalculationFactor, Formulas formula) {
+        for(var contextStatement : statement.getCloseContext()){
+            if(formula == Formulas.TARANTULA){
+                Double oldScore = contextStatement.getTarantula();
+                contextStatement.setTarantula(oldScore * recalculationFactor);
+            }
+            else if(formula == Formulas.OCHIAI){
+                Double oldScore = contextStatement.getOchiai();
+                contextStatement.setOchiai(oldScore * recalculationFactor);
+            }
+            else if(formula == Formulas.DSTAR){
+                //Double oldScore = contextStatement.getDStar();
+                //contextStatement.setDStar(oldScore * recalculationFactor);
+            }
+            else if(formula == Formulas.WONG2){
+                Double oldScore = contextStatement.getWong2();
+                contextStatement.setWong2(oldScore * recalculationFactor);
+            }
+        }
     }
 
     @Override
