649f4e775c5f85464d44163c0d5b8a9c506e51da
SzatmariA <szatma@inf.u-szeged.hu>
2022-11-03 09:08:04 +0100

    add dstar metric

--- src/main/python/faultloc/Metrics.py
+++ src/main/python/faultloc/Metrics.py
@@ -17,8 +17,9 @@ class Metrics:
             tarantula_score = self.tarantula(stats["ef"], stats["ep"], stats["nf"], stats["np"])
             ochiai_score = self.ochiai(stats["ef"], stats["ep"], stats["nf"], stats["np"])
             wong2_score = self.wong2(stats["ef"], stats["ep"], stats["nf"], stats["np"])
+            dstar_score = self.dstar(stats["ef"], stats["ep"], stats["nf"], stats["np"])
             self.scores_for_code_elements[code_element] = {"tar": tarantula_score, "och": ochiai_score,
-                                                           "wong2": wong2_score}
+                                                           "wong2": wong2_score, "dstar": dstar_score}
 
     def get_scores(self):
         return self.scores_for_code_elements
@@ -58,6 +59,20 @@ class Metrics:
         np = float(np)
         score = ef - ep
 
+        if score is None:
+            return 0.0
+        return round(score, 2)
+
+    def dstar(self, ef, ep, nf, np):
+        ef = float(ef)
+        ep = float(ep)
+        nf = float(nf)
+        np = float(np)
+        try:
+            score = math.pow(ef, 2) / (ef + nf)
+
+        except ZeroDivisionError:
+            score = 0.0
         if score is None:
             return 0.0
         return round(score, 2)
