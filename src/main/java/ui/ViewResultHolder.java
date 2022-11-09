f24e49ece3f87a83548934b7ee28b64f5df076fd
Soha <psoha@inf.u-szeged.hu>
2022-11-09 20:07:43 +0100

    Fixed the position of viewresult and fixed possible exceptions.

--- src/main/java/ui/ViewResultHolder.java
+++ src/main/java/ui/ViewResultHolder.java
@@ -1,15 +1,23 @@
 package ui;
 
+import java.awt.*;
+
 public class ViewResultHolder {
-    public static ViewResult view;
+    private static ViewResult view;
     static{
         view = new ViewResult();
+        view.setLocation(600,300);
     }
     public static void reOpen(){
+        Point point = view.getLocation();
+        System.out.println(point.toString());
+        int index = view.getSelectedIndex();
         if(null != view){
             view.close(0);
         }
         view = new ViewResult();
+        view.setLocation(point);
+        view.setSelectedIndex(index);
         view.show();
     }
 }
